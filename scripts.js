870cf2bce2fc967209a4759fc824061fbc95c3c4
isaedelman <edelmanisaac@gmail.com>
2024-06-06 11:54:37 -0700

    Fixed js expansion script. Updated projects.

--- scripts.js
+++ scripts.js
@@ -3,14 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     tiles.forEach(tile => {
         tile.addEventListener('click', function () {
-            if (this.classList.contains('expanded')) {
-                this.classList.remove('expanded');
-                this.style.maxHeight = '50px';
-            } else {
-                this.classList.add('expanded');
-                const contentHeight = this.querySelector('.tile-content').scrollHeight;
-                this.style.maxHeight = `${contentHeight + 40}px`; // Add some padding to ensure smooth transition
-            }
+            this.classList.toggle('expanded');
         });
     });
 });
